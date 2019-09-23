01241cbc550f2da061feeff954732b7e3872718e
DejanBanjac <banjacdmail@gmail.com>
2019-09-23 23:11:27 +0200

    Installed mongoose, mongodb and dotenv. Updated app.js to connect to Atlas mongodb cloud. Added start-dev script to run application in development mode

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+if(process.env.NODE_ENV === 'development') {
+    const dotenv = require('dotenv');
+    dotenv.config();
+}
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(3000);
+console.log(process.env.NODE_ENV);
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(result => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
